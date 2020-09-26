8fdf1a705b79b84a07e76a98dd64e372d3d4a4c7
napusuthar <napusuthar@gmail.com>
2020-09-26 17:03:00 +0530

    narpat-suthar-tech-solution

---
+++ code.js
@@ -0,0 +1,92 @@
+function sum(array){
+    return array.reduce((acc, value) => acc += value)
+}
+
+function getTargetSumArray(targetSum, ingredients){
+    return ingredients.map(i => targetSum);
+}
+
+function checkTargetSumArray(targetSumArray){
+    return targetSumArray.every(sum => {
+        if(!sum){
+            return false;
+        }
+        else{
+            return true;
+        }
+    })
+}
+
+function temp(curryArray, n, targetSumArray, curry){
+
+    if(!checkTargetSumArray(targetSumArray)){
+        return true;
+    }
+
+    if(n < 0){
+        return false;
+    }
+
+    let flag = false;
+
+    curryArray.forEach((e, i) => {
+        if(!flag && (targetSumArray[i] - curryArray[n] >= 0)){
+
+            curry[n] = i;
+            targetSumArray[i] = targetSumArray[i] - curryArray[n];
+            flag = temp(curryArray,
+                n-1,
+                targetSumArray,
+                curry).flag
+            targetSumArray[i] = targetSumArray[i] + curryArray[n];
+        }
+    });
+    return curry;
+}
+
+function makeCurry(curryArray){
+    let ingredients = ['P', 'Q', 'R'];
+    let ingredientLength = ingredients.length;
+    let curryArrayLength = curryArray.length;
+
+    let targetSum = sum(curryArray)/ingredientLength;
+    let targetSumArray = getTargetSumArray(targetSum, ingredients);
+
+    let curry = curryArray.map(c => false)
+
+    if(sum(curryArray)%ingredientLength !== 0){
+        return 'noLuck';
+    }
+
+    let result = temp(curryArray,
+        curryArrayLength - 1,
+        targetSumArray,
+        curry);
+
+    console.log(result);
+
+    if(!result.flag){
+        return 'noLuck';
+    }   
+
+    let result = '';
+
+    result.curry.forEach(c => {
+        if(c == 1){
+            result += 'P';
+        }
+        else if(c == 2){
+            result += 'Q';
+        }
+        else{
+            result += 'R';
+        }
+    })
+
+    return result;
+
+}
+
+var curryArray = [3, 7, 2, 5 ,4];
+makeCurry(curryArray);
+
